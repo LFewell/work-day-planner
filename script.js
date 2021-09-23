b94ad100de85ff976afb5e95ec327aaa7fffda85
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-23 12:25:01 -0400

    Starting writing the function for storing data and finished the day planner container

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Mo, h:mm a"));
+
+$(".saveBtn").on("click", storeData);
+
+function storeData() {
+  var key = $(".col-md").parent().attr("id");
+  var value = $(".col-md").val();
+  localStorage.setItem(key, value);
+}
+
+$("#9").val(localStorage.getItem("9"));
