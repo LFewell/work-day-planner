28f850bb8b757ad7ec38f72647a880b78edbe2d6
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-23 16:43:01 -0400

    Local storage is now working properly for each button, started writing function for changing color based on time

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Mo, h:mm a"));
 
 $(".saveBtn").on("click", storeData);
 
 function storeData() {
-  var key = $(".col-md").parent().attr("id");
-  var value = $(".col-md").val();
+  var key = $(this).parent().attr("id");
+  var value = $(this).siblings(".col-md").val();
   localStorage.setItem(key, value);
 }
 
 $("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+
+function colorChange() {
+    var currentTime = today.hours();
+    var divTime = parseInt($(".time-block").attr("id"));
+    console.log(divTime);
+    console.log(currentTime);    
+    if (divTime < currentTime) {
+        $(".time-block").addClass("past");
+        
+    } else if (divTime === currentTime) {
+        $(".time-block").removeClass("past");
+         $(this).removeClass("future");
+         $(this).addClass("present");
+    }
+}
+
+colorChange();
