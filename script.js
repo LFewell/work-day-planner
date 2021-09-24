2331f5ddefb33b17526956416a6515b6d215fe8d
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-24 14:39:38 -0400

    Fixed local storage retrieval

--- script.js
+++ script.js
@@ -10,8 +10,16 @@ function storeData() {
   localStorage.setItem(key, value);
 }
 
-$("#9").val(localStorage.getItem("9"));
-$("#10").val(localStorage.getItem("10"));
+$("#9 > textarea").val(localStorage.getItem("9"));
+$("#10 > textarea").val(localStorage.getItem("10"));
+$("#11 > textarea").val(localStorage.getItem("11"));
+$("#12 > textarea").val(localStorage.getItem("12"));
+$("#13 > textarea").val(localStorage.getItem("13"));
+$("#14 > textarea").val(localStorage.getItem("14"));
+$("#15 > textarea").val(localStorage.getItem("15"));
+$("#16 > textarea").val(localStorage.getItem("16"));
+$("#17 > textarea").val(localStorage.getItem("17"));
+
 
 
 function setColor() {
