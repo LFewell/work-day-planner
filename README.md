8c741c81f3d47460cee106f4a1425590c1ad9778
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-24 21:48:21 -0400

    Commented and cleaned up code, also added the README contents

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # work-day-planner
+This is a planner for a 9am-5pm work day.  Text can be saved using the save button on the right side.  The text area will change colors based on if the event is in the past, present, or future.
+
+![Screenshot of the deployed webpage](./work-day-planner.png)
+
+https://lfewell.github.io/work-day-planner/
--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-
+// Displays the current time and date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, h:mm a"));
 
+// Runs the local storage function when the save button is clicked
 $(".saveBtn").on("click", storeData);
-
+// Local storage function
 function storeData() {
   var key = $(this).parent().attr("id");
   var value = $(this).siblings(".col-md").val();
   localStorage.setItem(key, value);
 }
 
+// Grabs any locally stored data and displays it under the correct time
 $("#9 > textarea").val(localStorage.getItem("9"));
 $("#10 > textarea").val(localStorage.getItem("10"));
 $("#11 > textarea").val(localStorage.getItem("11"));
@@ -20,31 +22,28 @@ $("#15 > textarea").val(localStorage.getItem("15"));
 $("#16 > textarea").val(localStorage.getItem("16"));
 $("#17 > textarea").val(localStorage.getItem("17"));
 
-
-
+// Function for adding and removing the css classes that change the color based on the current time
 function setColor() {
+  var currentTime = today.hours();
 
-    var currentTime = today.hours();
+  for (var i = 0; i < $(".time-block").length; i++) {
+    var targetId = $(".time-block")[i].id;
+    var el = $(".time-block")[i];
+    console.log(targetId);
+    console.log(currentTime);
 
-    for (var i = 0; i < $(".time-block").length; i++) {
-        
-        var targetId = $(".time-block")[i].id;
-        var el = $(".time-block")[i];
-        console.log(targetId);
-        console.log(currentTime);
-
-        if (targetId < currentTime ) {
-            el.classList.add("past");
-        } else if (targetId == currentTime ) {
-            el.classList.remove("past");
-            el.classList.remove("future");
-            el.classList.add("present");
-        } else {
-            el.classList.add("future");
-            el.classList.remove("past")
-            el.classList.remove("present")
-        }
+    if (targetId < currentTime) {
+      el.classList.add("past");
+    } else if (targetId == currentTime) {
+      el.classList.remove("past");
+      el.classList.remove("future");
+      el.classList.add("present");
+    } else {
+      el.classList.add("future");
+      el.classList.remove("past");
+      el.classList.remove("present");
     }
+  }
 }
-
+// Calls the function for color change
 setColor();
